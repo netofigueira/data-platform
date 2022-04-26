7d102528bf578cfa029a8d59a76ad5c753547753
Neto <neto.gffn@gmail.com>
2022-04-26 12:09:32 -0300

    corrige imports

--- data_platform/data_lake/stack.py
+++ data_platform/data_lake/stack.py
@@ -1,11 +1,11 @@
 from data_platform.data_lake.base import BaseDataLakeBucket, DataLakeLayer
-from aws_cdk import core
+from aws_cdk import Stack, Construct, Duration
 from aws_cdk import (aws_s3 as s3, )
 from data_platform import active_environment
 
 
-class DataLakeStack(core.Stack):
-    def __init__(self, scope: core.Construct, **kwargs) -> None:
+class DataLakeStack(Stack):
+    def __init__(self, scope: Construct, **kwargs) -> None:
         self.deploy_env = active_environment
         super().__init__(scope, id='f{self.deploy_env.value}-data-lake-stack', **kwargs)
 
@@ -20,11 +20,11 @@ class DataLakeStack(core.Stack):
             transitions=[
                 s3.Transition(
                     storage_class=s3.StorageClass.INTELLIGENT_TIERING,
-                    transition_after=core.Duration.days(90),
+                    transition_after=Duration.days(90),
                 ),
                 s3.Transition(
                     storage_class=s3.StorageClass.GLACIER,
-                    transition_after=core.Duration.days(360)
+                    transition_after=Duration.days(360)
                     )
 
                 ],
